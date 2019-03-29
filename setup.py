7c14d50e87c7a3ad34b6962b446ae402b893adc0
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2019-03-29 16:25:47 +0100

    better Python deps

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup()
-d['packages'] = ['ecn_common']
-d['package_dir'] = {'': 'src'}
+d = generate_distutils_setup(packages=['ecn_common'], package_dir = {'': 'src'})
 
 setup(**d)
---
+++ src/ecn_common/__init__.py
@@ -0,0 +1,1 @@
+from .token_handle import TokenHandle
